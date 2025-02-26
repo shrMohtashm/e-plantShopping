b899f2c58c54f1564472e36d70f84d1f2f7030fd
shrMohtashm <shr.mohtsham@gmail.com>
2025-02-26 01:46:03 -0500

    change style

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -221,7 +221,6 @@ function ProductList() {
         }
     ];
     const styleObj = {
-        backgroundColor: '#4CAF50',
         color: '#fff!important',
         padding: '15px',
         display: 'flex',
@@ -261,6 +260,16 @@ function ProductList() {
             [product.name]: true
         }))
     }
+
+    useEffect(() => {
+        const updatedAddedToCart = {};
+        cartItems.forEach((item) => {
+            updatedAddedToCart[item.name] = true;
+        });
+        setAddedToCart(updatedAddedToCart);
+    }, [cartItems]);
+
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -278,13 +287,10 @@ function ProductList() {
 
                 </div>
                 <div style={styleObjUl}>
-                    <div>
-                        <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div>
-                        <div className="cart-icon" onClick={() => setShowCart(true)}>
-                            ({totalQuantity})
-                        </div>
-                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+
+                    <div>  <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><label style={{ zIndex: 1, position: "fixed", fontSize: "1.5rem", cursor: "pointer" }}> {totalQuantity}</label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">0<rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+
                 </div>
             </div>
             {!showCart ? (
@@ -293,7 +299,7 @@ function ProductList() {
                         plantsArray.length > 0 &&
                         plantsArray.map((category, index) => (
                             <div key={index}>
-                                <h1>
+                                <h1 style={{ textAlign: 'center' }}>
                                     {category.category}
                                 </h1>
                                 <div className='product-list'>
@@ -304,7 +310,12 @@ function ProductList() {
                                                 <div className='product-title'>
                                                     {plant.name}
                                                 </div>
-
+                                                <div>
+                                                    {plant.description}
+                                                </div>
+                                                <div>
+                                                    {plant.cost}
+                                                </div>
                                                 <button
                                                     className='product-button'
                                                     style={{ backgroundColor: addedToCart[plant.name] ? "gray" : "#615EFC" }}
